3047e1894f95acd51a15ec01019797499697a3f1
losmi09 <radic3000@gmail.com>
2025-12-17 00:43:55 +0100

    feat: handle unhandled routes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import AppError from './utils/appError.js';
+import globalErrorHandler from './controllers/errorController.js';
 
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 
-app.get('/', (req, res) => res.send('<h1>It Works!</h1>'));
+app.use((req, res, next) =>
+  next(new AppError('The requested resource was not found', 404))
+);
+
+app.use(globalErrorHandler);
 
 export default app;
