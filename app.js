6e3c101604de46a5609ff37b4329b1e2310bc159
losmi09 <radic3000@gmail.com>
2025-12-16 21:02:24 +0100

    feat: add body parser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json({ limit: '10kb' }));
+
 app.get('/', (req, res) => res.send('<h1>It Works!</h1>'));
 
 export default app;
