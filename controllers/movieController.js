dc771d6f19ae033e8e3a727b36ff3d371aebccbd
losmi09 <radic3000@gmail.com>
2025-12-19 18:25:42 +0100

    feat: add a controller function to get single movie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,12 +1,21 @@
 import catchAsync from '../utils/catchAsync.js';
 import movieSchema from '../schemas/movieSchema.js';
 import { throwValidationError } from './errorController.js';
+import AppError from '../utils/appError.js';
 import * as movieService from '../services/movieService.js';
 
 export const getAllMovies = catchAsync(async (req, res, next) => {
   const { movies, metaData } = await movieService.getAllMovies(req.query);
 
   res.status(200).json({ data: movies, meta: metaData });
+});
+
+export const getMovie = catchAsync(async (req, res, next) => {
+  const movie = await movieService.getMovie(req.params.id);
+
+  if (!movie) return next(new AppError('No movie found with this ID', 404));
+
+  res.status(200).json(movie);
 });
 
 export const createMovie = catchAsync(async (req, res, next) => {
