acf170c2141666223712b6f7d00888c79c2f2d53
losmi09 <radic3000@gmail.com>
2025-12-19 17:53:51 +0100

    feat: add repository function to get all movies

--- repositories/movieRepository.js
+++ repositories/movieRepository.js
@@ -1,4 +1,18 @@
+import prepareQuery from '../utils/query/prepareQuery.js';
 import prisma from '../server.js';
+
+export const getAllMovies = async query => {
+  const { queryClone, skip, limit, orderBy, selectFields } =
+    prepareQuery(query);
+
+  return await prisma.movie.findMany({
+    skip,
+    take: limit,
+    where: queryClone,
+    orderBy,
+    select: selectFields,
+  });
+};
 
 export const createMovie = async movieData =>
   await prisma.movie.create({ data: movieData });
