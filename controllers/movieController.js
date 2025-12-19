89f8bc0b4a7b4278887b498016cdff22b3c2e2e3
losmi09 <radic3000@gmail.com>
2025-12-19 17:54:27 +0100

    feat: add controller function to get all movies

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,6 +3,12 @@ import movieSchema from '../schemas/movieSchema.js';
 import { throwValidationError } from './errorController.js';
 import * as movieService from '../services/movieService.js';
 
+export const getAllMovies = catchAsync(async (req, res, next) => {
+  const { movies, metaData } = await movieService.getAllMovies(req.query);
+
+  res.status(200).json({ data: movies, meta: metaData });
+});
+
 export const createMovie = catchAsync(async (req, res, next) => {
   const { body: movieData } = req;
 
