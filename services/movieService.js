a3b1afff471c1e1b1467d60358333d15fdb17f8e
losmi09 <radic3000@gmail.com>
2025-12-19 17:54:08 +0100

    feat: add service function to get all movies

--- services/movieService.js
+++ services/movieService.js
@@ -1,4 +1,13 @@
 import * as movieRepository from '../repositories/movieRepository.js';
+import getMetaData from '../utils/query/getMetaData.js';
+
+export const getAllMovies = async query => {
+  const movies = await movieRepository.getAllMovies(query);
+
+  const metaData = await getMetaData(query, 'movie');
+
+  return { movies, metaData };
+};
 
 export const createMovie = async movieData =>
   await movieRepository.createMovie(movieData);
