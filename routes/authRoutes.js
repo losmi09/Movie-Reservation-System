40e6541f26bf4bdd3fecefe16c3584c3acd50c89
losmi09 <radic3000@gmail.com>
2025-12-20 23:01:10 +0100

    feat: add /refresh-token route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ import * as authController from '../controllers/authController.js';
 export const router = Router();
 
 router.post('/register', authController.register);
-
 router.post('/login', authController.login);
+
+router.post('/refresh-token', authController.refreshToken);
