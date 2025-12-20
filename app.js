ba10f8ed9751448b72b65f310a92fa5db750e509
losmi09 <radic3000@gmail.com>
2025-12-20 21:17:59 +0100

    feat: add an endpoint for auth routes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import AppError from './utils/appError.js';
 import globalErrorHandler from './controllers/errorController.js';
 import { router as movieRouter } from './routes/movieRoutes.js';
+import { router as authRouter } from './routes/authRoutes.js';
 
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 
 app.use('/api/v1/movies', movieRouter);
+app.use('/api/v1/auth', authRouter);
 
 app.use((req, res, next) =>
   next(new AppError('The requested resource was not found', 404))
