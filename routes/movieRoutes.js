49735d74138673985fc2d0139157afca7a7c33c2
losmi09 <radic3000@gmail.com>
2025-12-21 00:31:41 +0100

    feat: protect non-GET routes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import validateUuid from '../middlewares/validateUuid.js';
 import * as movieController from '../controllers/movieController.js';
+import * as authMiddleware from '../middlewares/auth.js';
 
 export const router = Router();
 
 router
   .route('/')
   .get(movieController.getAllMovies)
-  .post(movieController.createMovie);
+  .post(authMiddleware.protect, movieController.createMovie);
 
 router
   .route('/:id')
   .get(validateUuid, movieController.getMovie)
-  .patch(validateUuid, movieController.updateMovie)
-  .delete(validateUuid, movieController.deleteMovie);
+  .patch(validateUuid, authMiddleware.protect, movieController.updateMovie)
+  .delete(validateUuid, authMiddleware.protect, movieController.deleteMovie);
