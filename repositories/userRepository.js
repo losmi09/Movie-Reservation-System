b4c19400b541849aaf32d1b19a8ae41d86d3a71b
losmi09 <radic3000@gmail.com>
2025-12-20 21:44:23 +0100

    feat: add functions to find user by email and set refresh token

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -2,3 +2,9 @@ import prisma from '../server.js';
 
 export const createUser = async userData =>
   await prisma.user.create({ data: userData });
+
+export const findUserByEmail = async email =>
+  await prisma.user.findUnique({ where: { email } });
+
+export const setRefreshToken = async (userId, refreshToken) =>
+  await prisma.user.update({ where: { id: userId }, data: { refreshToken } });
