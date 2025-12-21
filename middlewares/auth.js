794fb690f1a75c0fc2ed9e4e8b3f2eaecb4f55fb
losmi09 <radic3000@gmail.com>
2025-12-21 01:43:05 +0100

    feat: add middleware for role based access

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -19,3 +19,14 @@ export const protect = catchAsync(async (req, res, next) => {
 
   next();
 });
+
+export const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role))
+      return next(
+        new AppError("You don't have permission to perform this action", 403)
+      );
+
+    next();
+  };
+};
