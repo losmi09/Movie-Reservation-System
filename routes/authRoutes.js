677bbde17f347dfd4ba0d2c7fc53aa8f81e97093
losmi09 <radic3000@gmail.com>
2025-12-21 04:42:47 +0100

    feat: add route for email verification

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,3 +7,5 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 
 router.post('/refresh-token', authController.refreshToken);
+
+router.patch('/verify-email/:token', authController.verifyEmail);
