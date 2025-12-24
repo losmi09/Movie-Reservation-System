cfd2811c8792ddceca1a93953ecbf8db9905ccae
losmi09 <radic3000@gmail.com>
2025-12-24 22:53:42 +0100

    feat: add routes for password reset and update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 import * as authController from '../controllers/authController.js';
+import * as authMiddleware from '../middlewares/auth.js';
 
 export const router = Router();
 
 router.post('/register', authController.register);
+
 router.post('/login', authController.login);
+
+router.patch('/verify-email/:token', authController.verifyEmail);
 
 router.post('/refresh-token', authController.refreshToken);
 
-router.patch('/verify-email/:token', authController.verifyEmail);
+router.post('/forgot-password', authController.forgotPassword);
+
+router.patch('/reset-password/:token', authController.resetPassword);
+
+router.use(authMiddleware.protect);
+
+router.patch('/me/password', authController.updateUserPassword);
