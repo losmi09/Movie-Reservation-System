257beef3c4f8b938b5ed0362f705687d0fa3af4e
losmi09 <radic3000@gmail.com>
2025-12-24 23:19:38 +0100

    feat: add rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 import cookieParser from 'cookie-parser';
 import AppError from './utils/appError.js';
 import globalErrorHandler from './controllers/errorController.js';
@@ -13,12 +14,24 @@ app.use(helmet());
 
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
+const limit = rateLimit({
+  max: Number(process.env.RATE_LIMIT_MAX),
+  windowMs: Number(process.env.RATE_LIMIT_WINDOW),
+  message: 'Too many requests. Please try again later',
+});
+
+const authLimit = rateLimit({
+  max: Number(process.env.AUTH_RATE_LIMIT_MAX),
+  windowMs: Number(process.env.AUTH_RATE_LIMIT_WINDOW),
+  message: 'Too many requests. Please try again later',
+});
+
 app.use(express.json({ limit: '10kb' }));
 
 app.use(cookieParser());
 
-app.use('/api/v1/movies', movieRouter);
-app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/movies', limit, movieRouter);
+app.use('/api/v1/auth', authLimit, authRouter);
 
 app.use((req, res, next) =>
   next(new AppError('The requested resource was not found', 404))
