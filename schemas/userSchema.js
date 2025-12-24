a15917397007c02cc6d164cd35a8b02b446e3e9d
losmi09 <radic3000@gmail.com>
2025-12-24 22:38:56 +0100

    feat: add separate schemas for email and password

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,14 +1,51 @@
 import Joi from 'joi';
 
-const userSchema = Joi.object({
-  firstName: Joi.string().required().trim(),
-  lastName: Joi.string().required().trim(),
-  email: Joi.string().email().required().trim(),
-  password: Joi.string().required().min(8).trim(),
-  passwordConfirm: Joi.any()
+const emailField = {
+  email: Joi.string().email().lowercase().required().trim(),
+};
+
+const passwordCurrentField = {
+  passwordCurrent: Joi.string().required().trim(),
+};
+
+const passwordField = {
+  password: Joi.string()
+    .invalid(Joi.ref('passwordCurrent'))
+    .required()
+    .messages({ 'any.invalid': 'New password cannot be the current one' })
+    .min(8)
+    .trim(),
+};
+
+const passwordConfirmField = {
+  passwordConfirm: Joi.string()
     .valid(Joi.ref('password'))
     .required()
     .messages({ 'any.only': 'Passwords do not match' }),
+};
+
+// Used for forgot-password
+export const emailSchema = Joi.object({ ...emailField });
+
+export const loginSchema = Joi.object({
+  ...emailField,
+  password: Joi.string().required().trim(),
+});
+
+// Used for password reset
+export const passwordSchema = Joi.object({
+  ...passwordCurrentField,
+  ...passwordField,
+  ...passwordConfirmField,
+});
+
+// Used for registration
+const userSchema = Joi.object({
+  firstName: Joi.string().required().trim(),
+  lastName: Joi.string().required().trim(),
+  ...emailField,
+  ...passwordField,
+  ...passwordConfirmField,
 });
 
 export default userSchema;
