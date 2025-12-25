ee2a59102f194ccf4a4df1c07a4856652248f611
losmi09 <radic3000@gmail.com>
2025-12-25 17:29:11 +0100

    feat: add route for user update

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import * as authMiddleware from '../middlewares/auth.js';
+import * as userController from '../controllers/userController.js';
+
+export const router = Router();
+
+router.use(authMiddleware.protect);
+
+router.patch('/me', userController.updateUser);
