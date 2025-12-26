a9d674133b4252ed033286ce746d831d0c69c44a
losmi09 <radic3000@gmail.com>
2025-12-26 20:33:21 +0100

    feat: add a route to get all cinemas

--- routes/cinemaRoutes.js
+++ routes/cinemaRoutes.js
@@ -1,6 +1,14 @@
 import { Router } from 'express';
 import * as cinemaController from '../controllers/cinemaController.js';
+import * as authMiddleware from '../middlewares/auth.js';
 
 export const router = Router();
 
-router.route('/').post(cinemaController.createCinema);
+router
+  .route('/')
+  .get(cinemaController.getAllCinemas)
+  .post(
+    authMiddleware.protect,
+    authMiddleware.restrictTo('admin'),
+    cinemaController.createCinema
+  );
