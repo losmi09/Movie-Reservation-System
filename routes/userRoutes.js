0177226eca7178b7cfb370aaab6cc6d51ef9501d
losmi09 <radic3000@gmail.com>
2025-12-27 20:44:17 +0100

    feat: add a photo upload route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import * as authMiddleware from '../middlewares/auth.js';
 import * as userController from '../controllers/userController.js';
+import * as photoMiddleware from '../middlewares/photo.js';
 
 export const router = Router();
 
 router.use(authMiddleware.protect);
 
+router.patch(
+  '/me/photo',
+  photoMiddleware.uploadUserPhoto,
+  photoMiddleware.resizeUserPhoto,
+  userController.saveUserPhoto
+);
 router
   .route('/me')
   .get(userController.getCurrentUser)
