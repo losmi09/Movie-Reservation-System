33a0ad1bda04b428001c6c5a55cd9fd741ab58b8
losmi09 <radic3000@gmail.com>
2025-12-29 18:34:50 +0100

    feat: add a hall controller

---
+++ controllers/hallController.js
@@ -0,0 +1,11 @@
+import * as crudController from './crudController.js';
+
+export const getAllHalls = crudController.getAll('hall');
+
+export const getHall = crudController.getOne('hall');
+
+export const createHall = crudController.createOne('hall');
+
+export const updatehall = crudController.updateOne('hall');
+
+export const deleteHall = crudController.deleteOne('hall');
