91f924505b634793e84401e4d14fac1db5777202
losmi09 <radic3000@gmail.com>
2025-12-30 21:37:46 +0100

    feat: add seatsPerRow field in hall schema

--- schemas/hallSchema.js
+++ schemas/hallSchema.js
@@ -1,8 +1,9 @@
 import Joi from 'joi';
 
 const hallSchema = Joi.object({
-  cinemaId: Joi.number().integer().positive(),
+  cinemaId: Joi.number().required().integer().positive(),
   name: Joi.string().required().trim(),
+  seatsPerRow: Joi.number().required().integer().positive().max(20),
 });
 
 export default hallSchema;
