5596c433005efcc12084ba1c9c7eb0862c90093f
losmi09 <radic3000@gmail.com>
2025-12-30 21:38:40 +0100

    feat: validate seat

--- utils/validateBody.js
+++ utils/validateBody.js
@@ -1,6 +1,8 @@
 import movieSchema from '../schemas/movieSchema.js';
 import cinemaSchema from '../schemas/cinemaSchema.js';
 import hallSchema from '../schemas/hallSchema.js';
+import seatSchema from '../schemas/seatSchema.js';
+import * as hallService from '../services/hallService.js';
 
 const excludeRequiredErrors = error =>
   error.details.filter(err => !err.message.endsWith('required'));
@@ -18,10 +20,30 @@ const validation = {
     const { error } = hallSchema.validate(body, { abortEarly: false });
     return error;
   },
+  seat: async body => {
+    let errorObj = { details: [], name: 'ValidationError' };
+
+    const { error } = seatSchema.validate(body, { abortEarly: false });
+
+    if (error) errorObj = error;
+
+    try {
+      if (await hallService.isSeatRowFull(body.hallId, body.row)) {
+        errorObj.details.push({
+          message: 'This row is full of seats',
+          path: 'row',
+        });
+      }
+    } catch {
+      return errorObj;
+    }
+
+    return errorObj.details.length ? errorObj : undefined;
+  },
 };
 
-const validateBody = (model, body, updating = false) => {
-  const error = validation[model](body);
+const validateBody = async (model, body, updating = false) => {
+  const error = await validation[model](body);
 
   if (!error) return;
 
