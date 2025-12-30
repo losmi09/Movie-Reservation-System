a50818bbdf8a0995bc5b8a38cf366a658ce89746
losmi09 <radic3000@gmail.com>
2025-12-30 21:45:50 +0100

    feat: add nested route for cinema halls

--- routes/cinemaRoutes.js
+++ routes/cinemaRoutes.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import validateId from '../middlewares/validateId.js';
 import * as cinemaController from '../controllers/cinemaController.js';
 import * as authMiddleware from '../middlewares/auth.js';
-import hallRouter from './hallRoutes.js';
+import { router as hallRouter } from './hallRoutes.js';
 
 export const router = Router();
 
-router.use('/:cinemaId/halls', validateId, hallRouter);
+router.use('/:id/halls', validateId, hallRouter);
 
 router
   .route('/')
@@ -16,6 +16,8 @@ router
     authMiddleware.restrictTo('admin'),
     cinemaController.createCinema
   );
+
+router.use(authMiddleware.protect);
 
 router
   .route('/:id')
