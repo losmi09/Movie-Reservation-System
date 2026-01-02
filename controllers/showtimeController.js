ca8b20f5b531ac536c68fa87c8b3472d30c6c602
losmi09 <radic3000@gmail.com>
2026-01-02 13:22:06 +0100

    fix: use updateOne instead of getOne function to update a showtime

--- controllers/showtimeController.js
+++ controllers/showtimeController.js
@@ -6,6 +6,6 @@ export const getShowtime = crudController.getOne('showtime');
 
 export const createShowtime = crudController.createOne('showtime');
 
-export const updateShowtime = crudController.getOne('showtime');
+export const updateShowtime = crudController.updateOne('showtime');
 
 export const deleteShowtime = crudController.deleteOne('showtime');
