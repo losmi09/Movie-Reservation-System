3d00dccc383de66a6ef0fd52e93723ac9653c32d
losmi09 <radic3000@gmail.com>
2026-01-02 05:30:54 +0100

    feat: add showtime controller

---
+++ controllers/showtimeController.js
@@ -0,0 +1,11 @@
+import * as crudController from './crudController.js';
+
+export const getAllShowtimes = crudController.getAll('showtime');
+
+export const getShowtime = crudController.getOne('showtime');
+
+export const createShowtime = crudController.createOne('showtime');
+
+export const updateShowtime = crudController.getOne('showtime');
+
+export const deleteShowtime = crudController.deleteOne('showtime');
