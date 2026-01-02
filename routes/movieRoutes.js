0b57319507231fea20a09b98685bd7f60abada93
losmi09 <radic3000@gmail.com>
2026-01-02 05:44:54 +0100

    feat: add a nested route for movie showtimes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import validateId from '../middlewares/validateId.js';
+import checkIfExists from '../middlewares/checkIfExists.js';
 import * as movieController from '../controllers/movieController.js';
 import * as authMiddleware from '../middlewares/auth.js';
+import { router as showtimeRouter } from './showtimeRoutes.js';
 
 export const router = Router();
+
+router.use(
+  '/:id/showtimes',
+  validateId,
+  checkIfExists('movie'),
+  showtimeRouter
+);
 
 router
   .route('/')
