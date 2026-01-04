bfa86e4529788914ce701a55b9675bf48f542d47
losmi09 <radic3000@gmail.com>
2026-01-04 14:40:25 +0100

    refactor: use nullish coalescing operator to check if port is defined

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from './app.js';
 
 const prisma = new PrismaClient();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT ?? 8000;
 
 app.listen(port, () => `Server running on port ${port}...`);
 
