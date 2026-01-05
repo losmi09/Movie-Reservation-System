3ed3b1bd40f7f9d8306875de3ecaafa2993bfeec
losmi09 <radic3000@gmail.com>
2026-01-05 07:50:42 +0100

    feat: add a nested route for user reservations

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,17 @@ import { Router } from 'express';
 import * as authMiddleware from '../middlewares/auth.js';
 import * as userController from '../controllers/userController.js';
 import * as photoMiddleware from '../middlewares/photo.js';
+import { secondRouter as reservationRouter } from './reservationRoutes.js';
 
 export const router = Router();
+
+router.use(authMiddleware.protect);
+
+router.use(
+  '/me/reservations',
+  authMiddleware.restrictTo('user'),
+  reservationRouter
+);
 
 router.use(authMiddleware.protect);
 
@@ -13,6 +22,7 @@ router.patch(
   photoMiddleware.resizeUserPhoto,
   userController.saveUserPhoto
 );
+
 router
   .route('/me')
   .get(userController.getCurrentUser)
