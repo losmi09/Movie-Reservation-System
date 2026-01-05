5f4e138a824fc453b969a71ea263f8f29be687ad
losmi09 <radic3000@gmail.com>
2026-01-05 11:52:54 +0100

    feat: add a route to upload movie poster image

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import validateId from '../middlewares/validateId.js';
+import checkIfExists from '../middlewares/checkIfExists.js';
 import setParentFilter from '../middlewares/setParentFilter.js';
 import * as movieController from '../controllers/movieController.js';
 import * as authMiddleware from '../middlewares/auth.js';
+import * as imageMiddleware from '../middlewares/image.js';
 import { router as showtimeRouter } from './showtimeRoutes.js';
 
 export const router = Router();
@@ -38,3 +40,14 @@ router
     authMiddleware.restrictTo('admin'),
     movieController.deleteMovie
   );
+
+router.patch(
+  '/:id/poster',
+  authMiddleware.protect,
+  authMiddleware.restrictTo('admin'),
+  validateId,
+  checkIfExists('movie'),
+  imageMiddleware.uploadMoviePoster,
+  imageMiddleware.resizeImage('movie'),
+  movieController.saveMoviePoster
+);
