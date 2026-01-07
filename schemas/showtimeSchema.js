1da4373ccea5df5c4a819fd3dc7719a5140ff71e
losmi09 <radic3000@gmail.com>
2026-01-07 12:19:11 +0100

    fix: add language field in showtime schema

--- schemas/showtimeSchema.js
+++ schemas/showtimeSchema.js
@@ -6,6 +6,7 @@ const showtimeSchema = Joi.object({
   hallId: Joi.number().required().integer().positive(),
   startTime: Joi.date().required().greater('now'),
   endTime: Joi.date().required().min(Joi.ref('startTime')),
+  language: Joi.string().trim(),
   price: Joi.number().positive().required(),
 });
 
