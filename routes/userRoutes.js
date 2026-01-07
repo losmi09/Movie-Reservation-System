fdae1b0d568e5434c9d31e0c26c6c62e9cff318c
losmi09 <radic3000@gmail.com>
2026-01-07 17:01:34 +0100

    refactor: rename router to userRouter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,28 +2,26 @@ import { Router } from 'express';
 import * as authMiddleware from '../middlewares/auth.js';
 import * as userController from '../controllers/userController.js';
 import * as imageMiddleware from '../middlewares/image.js';
-import { secondRouter as reservationRouter } from './reservationRoutes.js';
+import { userReservationRouter as reservationRouter } from './reservationRoutes.js';
 
-export const router = Router();
+export const userRouter = Router();
 
-router.use(authMiddleware.protect);
+userRouter.use(authMiddleware.protect);
 
-router.use(
+userRouter.use(
   '/me/reservations',
   authMiddleware.restrictTo('user'),
   reservationRouter
 );
 
-router.use(authMiddleware.protect);
-
-router.patch(
+userRouter.patch(
   '/me/photo',
   imageMiddleware.uploadUserPhoto,
   imageMiddleware.resizeImage('photo'),
   userController.saveUserPhoto
 );
 
-router
+userRouter
   .route('/me')
   .get(userController.getCurrentUser)
   .patch(userController.updateCurrentUser)
