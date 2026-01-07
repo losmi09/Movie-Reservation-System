22c306f13fba335a60ef727eceb8d42b0bdd41b0
losmi09 <radic3000@gmail.com>
2026-01-07 12:46:11 +0100

    feat: set up redis client

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import { PrismaClient } from '@prisma/client';
+import { createClient } from 'redis';
 import app from './app.js';
 
 const prisma = new PrismaClient();
+
+const cachingClient = async () => {
+  try {
+    return await createClient().connect();
+  } catch (err) {
+    console.error('Redis Client Error: ', err.message);
+  }
+};
+
+export const redisClient = await cachingClient();
 
 const port = process.env.PORT ?? 8000;
 
