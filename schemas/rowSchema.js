e85f0b51b3395fbe4a5db9ff0816c93b92ba00ad
losmi09 <radic3000@gmail.com>
2026-01-09 20:18:52 +0100

    feat: add row schema

---
+++ schemas/rowSchema.js
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+
+const rowSchema = Joi.object({
+  label: Joi.string()
+    .required()
+    .trim()
+    .valid('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'),
+  seatCapacity: Joi.number().required().positive().integer().max(20),
+  hallId: Joi.number().positive().integer(),
+});
+
+export default rowSchema;
