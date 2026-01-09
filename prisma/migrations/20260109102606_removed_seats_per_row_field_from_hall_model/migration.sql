d39cfb6cb296b991b9f9950c1618d0b213921e03
losmi09 <radic3000@gmail.com>
2026-01-09 11:46:05 +0100

    chore: remove seatsPerRow field from Hall model

---
+++ prisma/migrations/20260109102606_removed_seats_per_row_field_from_hall_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `seats_per_row` on the `halls` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "halls" DROP COLUMN "seats_per_row";
