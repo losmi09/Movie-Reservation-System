92d822d9f4b06dc54483a223a4abd64021e331c7
losmi09 <radic3000@gmail.com>
2026-01-09 20:20:23 +0100

    fix: add rowId field in seat schema

--- schemas/seatSchema.js
+++ schemas/seatSchema.js
@@ -1,11 +1,7 @@
 import Joi from 'joi';
 
 const seatSchema = Joi.object({
-  hallId: Joi.number().required().integer().positive(),
-  row: Joi.string()
-    .required()
-    .trim()
-    .valid('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'),
+  rowId: Joi.number().positive().integer(),
   number: Joi.number().required().integer().positive().max(20),
 });
 
