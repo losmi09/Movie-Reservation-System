78a3bb9829ec23ddf1bf5f437c9fbba3a68bb3b0
losmi09 <radic3000@gmail.com>
2026-01-11 14:51:30 +0100

    feat: add caching middlewares

--- routes/rowRoutes.js
+++ routes/rowRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import checkIfExists from '../middlewares/checkIfExists.js';
 import validateId from '../middlewares/validateId.js';
+import { cacheAll, cacheOne } from '../middlewares/caching.js';
 import * as rowController from '../controllers/rowController.js';
 import * as authMiddleware from '../middlewares/auth.js';
 import { rowSeatRouter } from './seatRoutes.js';
@@ -14,7 +15,7 @@ rowRouter.use('/:id/seats', validateId, rowSeatRouter);
 
 rowRouter
   .route('/:id')
-  .get(rowController.getRow)
+  .get(cacheOne('row'), rowController.getRow)
   .patch(authMiddleware.restrictTo('admin'), rowController.updateRow)
   .delete(authMiddleware.restrictTo('admin'), rowController.deleteRow);
 
@@ -25,5 +26,5 @@ hallRowRouter.use(authMiddleware.protect);
 
 hallRowRouter
   .route('/')
-  .get(checkIfExists('hall'), rowController.getAllRows)
+  .get(checkIfExists('hall'), cacheAll('row'), rowController.getAllRows)
   .post(authMiddleware.restrictTo('admin'), rowController.createRow);
