2cc57017125ba16c50177f867325e2daab423800
losmi09 <radic3000@gmail.com>
2026-01-11 13:19:09 +0100

    feat: add reservation repository

---
+++ repositories/reservationRepository.js
@@ -0,0 +1,32 @@
+import prisma from '../server.js';
+
+export const getReservationById = async reservationId =>
+  await prisma.reservation.findUnique({
+    where: { id: reservationId, status: 'reserved' },
+  });
+
+export const getReservation = async (showtimeId, seatId) =>
+  await prisma.reservation.findFirst({
+    where: { showtimeId, seatId, status: 'reserved' },
+  });
+
+export const countShowtimeReservations = async showtimeId =>
+  await prisma.reservation.count({ where: { showtimeId, status: 'reserved' } });
+
+export const getFirstInWaitlist = async () =>
+  await prisma.reservation.findFirst({
+    where: { status: 'waitlist' },
+    orderBy: { id: 'asc' },
+  });
+
+export const cancelReservation = reservationId =>
+  prisma.reservation.update({
+    where: { id: reservationId },
+    data: { status: 'cancelled' },
+  });
+
+export const reserveSeatForFirstInWaitlist = (waitlistId, seatId) =>
+  prisma.reservation.update({
+    where: { id: waitlistId },
+    data: { seatId, status: 'reserved' },
+  });
