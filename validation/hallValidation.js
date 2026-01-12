deb0b7d6c26caafbc3d567e2c0b0e0fa8d1f5df0
losmi09 <radic3000@gmail.com>
2026-01-12 23:24:27 +0100

    refactor: move hall validation to separate file

---
+++ validation/hallValidation.js
@@ -0,0 +1,15 @@
+import hallSchema from '../schemas/hallSchema.js';
+import checkIfParentExists from '../utils/validation/checkIfParentExists.js';
+import validateSchema from '../utils/validation/validateSchema.js';
+
+const validateHall = async (data, isUpdating) => {
+  const errorObj = validateSchema(hallSchema, data, isUpdating);
+
+  // Verify that parent cinema exists
+  if (!isUpdating) await checkIfParentExists('cinema', data.cinemaId, errorObj);
+
+  // If there are no validation errors, simply return undefined
+  return errorObj.details.length ? errorObj : undefined;
+};
+
+export default validateHall;
