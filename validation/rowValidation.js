e09f5a20d2b2e587c16d2442ff2ecc7310d4b62e
losmi09 <radic3000@gmail.com>
2026-01-12 23:25:00 +0100

    refactor: move row validation to separate file

---
+++ validation/rowValidation.js
@@ -0,0 +1,24 @@
+import rowSchema from '../schemas/rowSchema.js';
+import pushValidationError from '../utils/validation/pushValidationError.js';
+import validateSchema from '../utils/validation/validateSchema.js';
+import checkIfParentExists from '../utils/validation/checkIfParentExists.js';
+import * as hallService from '../services/hallService.js';
+
+const validateRow = async (data, isUpdating) => {
+  const errorObj = validateSchema(rowSchema, data, isUpdating);
+
+  if (!isUpdating) {
+    // Verify that parent hall exists
+    const { hallId } = data;
+
+    const hall = await checkIfParentExists('hall', hallId, errorObj);
+
+    // Verify that hall is not full of rows
+    if (hall && (await hallService.isHallFullOfRows(hallId)))
+      pushValidationError(errorObj, 'hallId', 'This hall is full of rows');
+  }
+
+  return errorObj.details.length ? errorObj : undefined;
+};
+
+export default validateRow;
