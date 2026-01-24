167089e22ba168b804ad28f3b787e0f19de04cab
losmi09 <radic3000@gmail.com>
2026-01-24 23:53:16 +0100

    fix: validate UUID instead of ID

--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -1,7 +1,8 @@
+import { validate as validateUuid } from 'uuid';
 import AppError from '../utils/appError.js';
 
 const validateId = (req, res, next) => {
-  if (!Number.isInteger(Number(req.params.id)))
+  if (!validateUuid(req.params.id))
     return next(new AppError('Invalid ID', 400));
 
   next();
