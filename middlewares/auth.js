848eb07d15c89bf30cca6411a832c04b7e087ba1
losmi09 <radic3000@gmail.com>
2026-02-03 23:00:07 +0100

    refactor: use destructuring to extract access token from authorization header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,11 +6,11 @@ export const protect = catchAsync(async (req, res, next) => {
   let accessToken;
 
   if (req.headers?.authorization?.startsWith('Bearer'))
-    accessToken = req.headers.authorization.split(' ')[1];
+    [, accessToken] = req.headers.authorization.split(' ');
 
   if (!accessToken)
     return next(
-      new AppError("You're logged out. Please log in to get access", 401)
+      new AppError("You're logged out. Please log in to get access", 401),
     );
 
   const user = await authService.protect(accessToken);
@@ -24,7 +24,7 @@ export const restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role))
       return next(
-        new AppError("You don't have permission to perform this action", 403)
+        new AppError("You don't have permission to perform this action", 403),
       );
 
     next();
