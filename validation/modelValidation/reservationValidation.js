afc664393165a9ce28519ea0bbdf62034ab9314b
losmi09 <radic3000@gmail.com>
2026-02-25 17:44:56 +0100

    feat: check if the user has reserved the maximum number of seats allowed

--- validation/modelValidation/reservationValidation.js
+++ validation/modelValidation/reservationValidation.js
@@ -2,16 +2,17 @@ import reservationSchema from '../../schemas/reservationSchema.js';
 import validateSchema from '../utils/validateSchema.js';
 import pushValidationError from '../utils/pushValidationError.js';
 import checkIfForeignKeysAreValid from '../utils/checkIfForeignKeysAreValid.js';
+import throwValidationError from '../utils/throwValidationError.js';
 import * as crudRepository from '../../repositories/crudRepository.js';
 import * as showtimeService from '../../services/showtimeService.js';
 import * as reservationService from '../../services/reservationService.js';
 
-const validateReservation = async (body, isUpdating, reservationId) => {
+const validateReservation = async (body, isUpdating, reservationId, userId) => {
   const errorObj = validateSchema(reservationSchema, body, isUpdating);
 
   let { showtimeId, seatId } = body;
 
-  // Updating = reservation cancellation
+  // Updating = reservation cancellation, as no fields except status can be updated
   if (isUpdating) {
     // When udating, we get showtimeId by destructuring it from current reservation
     ({ showtimeId, seatId } = await crudRepository.getOne(
@@ -24,11 +25,7 @@ const validateReservation = async (body, isUpdating, reservationId) => {
   const showtime = await crudRepository.getOne('showtime', showtimeId);
 
   if (!showtime)
-    pushValidationError(
-      errorObj,
-      'showtimeId',
-      'No showtime found with this ID',
-    );
+    return throwValidationError('showtimeId', 'No showtime found with this ID');
 
   // Verify that showtime has not started/finished
   const closedReservations = new Date(showtime?.startTime) < new Date();
@@ -40,37 +37,49 @@ const validateReservation = async (body, isUpdating, reservationId) => {
       ? 'You cannot cancel a reservation for a showtime that is ongoing or has ended'
       : 'Reservations are closed for this showtime';
 
-    pushValidationError(errorObj, field, errorMessage);
+    return throwValidationError(field, errorMessage);
   }
 
-  // Verify that seatId is provided and that it is a valid UUID
-  if (seatId && checkIfForeignKeysAreValid(errorObj.details)) {
-    // Verify that seat exists
-    const seat = await crudRepository.getOne('seat', seatId);
+  if (!isUpdating) {
+    // Check if the user has reserved the maximum number of seats allowed
+    const RESERVED_SEATS_LIMIT = 5;
 
-    if (!seat)
-      pushValidationError(errorObj, 'seatId', 'No seat found with this ID');
+    const reservedSeatsByUser =
+      await reservationService.countShowtimeReservations(showtimeId, userId);
 
-    if (seat) {
-      const [row, reservedSeat, allSeatsReserved] = await Promise.all([
-        crudRepository.getOne('row', seat.rowId),
-        reservationService.getReservation(showtimeId, seatId),
-        showtimeService.areAllSeatsReserved(showtimeId, showtime?.hallId),
-      ]);
-
-      // Verify that seat belongs to hall with provided ID
-      if (showtime && row.hallId !== showtime.hallId)
-        pushValidationError(
-          errorObj,
-          'seatId',
-          'Seat with this ID does not belong to this hall',
-        );
-
-      // Verify that seat is not reserved
-      if (reservedSeat && !allSeatsReserved)
-        pushValidationError(errorObj, 'seatId', 'This seat is reserved');
-    }
+    if (reservedSeatsByUser >= RESERVED_SEATS_LIMIT)
+      return throwValidationError(
+        'reservationLimit',
+        `You cannot reserve more than ${RESERVED_SEATS_LIMIT} seats`,
+      );
   }
+
+  // Ensure seatId is provided and foreign keys are valid
+  if (!seatId || !checkIfForeignKeysAreValid(errorObj.details)) return errorObj;
+
+  // Verify that seat exists
+  const seat = await crudRepository.getOne('seat', seatId);
+
+  if (!seat)
+    return throwValidationError('seatId', 'No seat found with this ID');
+
+  const [row, reservedSeat, allSeatsReserved] = await Promise.all([
+    crudRepository.getOne('row', seat.rowId),
+    reservationService.getReservation(showtimeId, seatId),
+    showtimeService.areAllSeatsReserved(showtimeId, showtime?.hallId),
+  ]);
+
+  // Verify that seat belongs to hall with provided ID
+  if (showtime && row.hallId !== showtime.hallId)
+    pushValidationError(
+      errorObj,
+      'seatId',
+      'Seat with this ID does not belong to this hall',
+    );
+
+  // Verify that seat is not reserved
+  if (reservedSeat && !allSeatsReserved)
+    pushValidationError(errorObj, 'seatId', 'This seat is reserved');
 
   return errorObj.details.length ? errorObj : undefined;
 };
