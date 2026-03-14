474ad7971ccf6455c1936df4cfc716f788e351e6
losmi09 <radic3000@gmail.com>
2026-03-14 20:58:36 +0100

    refactor: add functions to create and update the movie

--- repositories/movieRepository.js
+++ repositories/movieRepository.js
@@ -1,4 +1,9 @@
-import prisma from '../server.js';
+import { prisma } from '../server.js';
+
+export const createMovie = async data => await prisma.movie.create({ data });
+
+export const updateMovie = async (movieId, data) =>
+  await prisma.movie.update({ where: { id: movieId }, data });
 
 export const saveMoviePoster = async (movieId, fileName) =>
   await prisma.movie.update({
--- services/movieService.js
+++ services/movieService.js
@@ -1,4 +1,30 @@
+import slugify from 'slugify';
+import * as redisService from '../services/redisService.js';
 import * as movieRepository from '../repositories/movieRepository.js';
 
-export const saveMoviePoster = async (movieId, fileName) =>
-  await movieRepository.saveMoviePoster(movieId, fileName);
+export const createMovie = async data => {
+  const movie = movieRepository.createMovie({
+    ...data,
+    slug: slugify(data.title, { lower: true }),
+  });
+
+  await redisService.invalidateCache('movie');
+
+  return movie;
+};
+
+export const updateMovie = async (movieId, data) => {
+  const { title } = data;
+
+  const updatedMovie = movieRepository.updateMovie(movieId, {
+    ...data,
+    ...(title && { slug: slugify(title, { lower: true }) }),
+  });
+
+  await redisService.invalidateCache('movie');
+
+  return updatedMovie;
+};
+
+export const saveMoviePoster = (movieId, fileName) =>
+  movieRepository.saveMoviePoster(movieId, fileName);
