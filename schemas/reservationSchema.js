bf2258fe47e36c9f28bbec40ba16a9e717e1cd76
losmi09 <radic3000@gmail.com>
2026-03-20 23:36:53 +0100

    refactor: remove userId from schema

--- schemas/reservationSchema.js
+++ schemas/reservationSchema.js
@@ -3,5 +3,4 @@ import Joi from 'joi';
 export const reservationSchema = Joi.object({
   showtimeId: Joi.string().trim().required().guid(),
   seatId: Joi.string().trim().required().guid(),
-  userId: Joi.string().trim().required().guid(),
 });
--- schemas/reviewSchema.js
+++ schemas/reviewSchema.js
@@ -2,7 +2,6 @@ import Joi from 'joi';
 
 export const reviewSchema = Joi.object({
   movieId: Joi.string().trim().required().guid(),
-  userId: Joi.string().trim().required().guid(),
   comment: Joi.string().trim().required(),
   rating: Joi.number().positive().integer().required().min(1).max(5),
 });
