9331d29731b732631562ced82687610f858e7bf6
losmi09 <radic3000@gmail.com>
2026-03-21 01:53:46 +0100

    refactor: remove cinemaId from hall schema

--- schemas/hallSchema.js
+++ schemas/hallSchema.js
@@ -1,7 +1,6 @@
 import Joi from 'joi';
 
 export const hallSchema = Joi.object({
-  cinemaId: Joi.string().trim().required().guid(),
   name: Joi.string().required().trim(),
   maxRows: Joi.number().required().positive().integer().min(5).max(20),
 });
