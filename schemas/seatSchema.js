7410b072fb0b397a322c2389ddf64c567a9a8d67
losmi09 <radic3000@gmail.com>
2026-03-21 01:51:32 +0100

    refactor: remove rowId from seat schema

--- schemas/seatSchema.js
+++ schemas/seatSchema.js
@@ -1,6 +1,5 @@
 import Joi from 'joi';
 
 export const seatSchema = Joi.object({
-  rowId: Joi.string().trim().required().guid(),
   number: Joi.number().required().integer().positive().max(20),
 });
