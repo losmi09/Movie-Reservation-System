6f3beff6f4c00c0d1c7193b17905c152da47884e
losmi09 <radic3000@gmail.com>
2026-04-04 00:43:39 +0200

    feat: implement cursor-based pagination

--- repositories/crudRepository.js
+++ repositories/crudRepository.js
@@ -1,35 +1,48 @@
-import { selectForManyDocs } from './prismaSelects.js';
-import * as queryService from '../services/queryService.js';
 import { prisma } from '../server.js';
 
-export const count = async (model, filters) =>
-  await prisma[model].count({ where: filters });
+export const getAll = async (
+  model,
+  decodedCursorToken,
+  pageSize,
+  filters,
+  selectFields,
+) => {
+  const { cursorCreatedAt, cursorId } = decodedCursorToken ?? {};
 
-export const getAll = async (model, query) => {
-  const { filters, skip, limit, orderBy, selectFields } =
-    queryService.prepareQuery(query);
+  let cursorQuery;
 
-  // null selects all fields
-  const select = selectFields ?? selectForManyDocs?.[model]?.() ?? null;
+  if (cursorCreatedAt && cursorId)
+    cursorQuery = {
+      OR: [
+        { createdAt: { gt: cursorCreatedAt } },
+        {
+          AND: [
+            { createdAt: { equals: cursorCreatedAt } },
+            { id: { gt: cursorId } },
+          ],
+        },
+      ],
+    };
 
   return await prisma[model].findMany({
-    skip,
-    take: limit,
-    where: filters,
-    orderBy,
-    select,
+    // Fetch one extra record to determine if there is a next page
+    take: pageSize + 1,
+    where: { ...filters, ...cursorQuery },
+    orderBy: [{ createdAt: 'asc' }, { id: 'asc' }],
+    // Always include id and createdAt to generate the next cursor
+    select: { ...selectFields, id: true, createdAt: true },
   });
 };
 
 // "baseConfig" refers to the object that would be passed without select config
-const selectParents = (baseConfig, select) =>
+const withSelect = (baseConfig, select) =>
   select ? { ...baseConfig, select } : baseConfig;
 
 export const getOne = async (model, id, select) =>
-  await prisma[model].findUnique(selectParents({ where: { id } }, select));
+  await prisma[model].findUnique(withSelect({ where: { id } }, select));
 
 export const updateOne = async (model, id, data, select) =>
-  await prisma[model].update(selectParents({ where: { id }, data }, select));
+  await prisma[model].update(withSelect({ where: { id }, data }, select));
 
 export const deleteOne = async (model, id) =>
   await prisma[model].delete({ where: { id } });
--- services/crudService.js
+++ services/crudService.js
@@ -1,15 +1,61 @@
 import { selectSingleDoc } from '../repositories/prismaSelects.js';
+import { selectForManyDocs } from '../repositories/prismaSelects.js';
 import * as crudRepository from '../repositories/crudRepository.js';
 import * as redisService from '../services/redisService.js';
+import * as queryService from '../services/queryService.js';
 import * as paginationService from '../services/paginationService.js';
 
-export const getAll = async (model, query) => {
-  const [data, meta] = await Promise.all([
-    crudRepository.getAll(model, query),
-    paginationService.getMetaData(model, query),
-  ]);
+const filterData = (data, allowedFields) =>
+  data.map(obj => {
+    const newObj = {};
 
-  return { data, meta };
+    Object.keys(obj).forEach(field => {
+      if (allowedFields.includes(field)) newObj[field] = obj[field];
+    });
+
+    return newObj;
+  });
+
+export const getAll = async (model, query) => {
+  const { cursor, pageSize, filters, selectFields } =
+    queryService.prepareQuery(query);
+
+  const decodedCursorToken = cursor
+    ? paginationService.decodeCursorToken(cursor)
+    : null;
+
+  // Select fields that were specified in query or in model's select-config
+  const select = selectFields ?? selectForManyDocs?.[model]?.();
+
+  const data = await crudRepository.getAll(
+    model,
+    decodedCursorToken,
+    pageSize,
+    filters,
+    select,
+  );
+
+  // data objects always contain createdAt and id fields (neccesary to determine next cursor), so if they are not specified in the select object - simply ignore them
+  const allowedFields = Object.keys(select);
+
+  let filteredData = filterData(data, allowedFields);
+
+  let nextCursor = null;
+
+  // Check if there are more records than the pageSize and if so - provide the next cursor
+  const hasNextPage = data.length > pageSize;
+
+  if (hasNextPage) {
+    // Get createdAt and id from last document - based on pageSize
+    const { createdAt, id } = data[pageSize - 1];
+
+    nextCursor = paginationService.encodeCursorToken(createdAt, id);
+
+    // Remove the extra record used to determine if next page exists
+    filteredData = filteredData.slice(0, -1);
+  }
+
+  return { data: filteredData, meta: { nextCursor } };
 };
 
 const selectConfig = model => selectSingleDoc?.[model]?.();
